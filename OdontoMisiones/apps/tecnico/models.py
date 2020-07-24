6d9a1313d9a8d0277f3c615590e39934faa1fd8a
Hernan Vieira <hernanvieira1997@gmail.com>
2020-07-23 22:12:33 -0300

    Clase Tecnico

--- OdontoMisiones/apps/tecnico/models.py
+++ OdontoMisiones/apps/tecnico/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Tecnico(models.Model):
+    id_tecnico = models.AutoField(primary_key=True)
+    apellido = models.CharField('apellido',max_length=100, blank=False, null=False)
+    nombre = models.CharField('nombre',max_length=100, blank=False, null=False)
+    usuario = models.CharField('usuario',unique=True, max_length=100, blank=False, null=False)
+    telefono = models.CharField('telefono',max_length=50)
+    correo = models.EmailField('correo')
+    direccion = models.CharField('domicilio',blank = True, null = True, max_length=500)
+    observaciones = models.TextField('observaciones',blank = True, null = True)
+
+    def __str__(self):
+        return str (self.apellido) + ' ' + self.nombre
