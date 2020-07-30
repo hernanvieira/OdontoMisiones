eab01e6056272262e69ed23b0801f8e16037ef61
Hernan Vieira <hernanvieira1997@gmail.com>
2020-07-30 00:25:38 -0300

    Bloques de index.html y crear_pais.html

--- OdontoMisiones/apps/configuracion/urls.py
+++ OdontoMisiones/apps/configuracion/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from apps.configuracion.views import crear_pais
 
 urlpatterns = [
+    path ('crear_pais/',crear_pais,name='crear_pais')
 ]
--- OdontoMisiones/apps/configuracion/views.py
+++ OdontoMisiones/apps/configuracion/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def Home(request):
     return render(request, 'index.html')
+
+def crear_pais(request):
+    return render(request, 'configuracion/crear_pais.html')
